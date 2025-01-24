46ce7a14135bcf3c344191c759e32f91c72eddeb
admin <vutukururevanth@gmail.com>
2025-01-23 21:44:03 -0800

    Day04

---
+++ Day04/RevanthException/src/main/java/Custom/RevanthAgeValidationException.java
@@ -0,0 +1,8 @@
+package Custom;
+
+public class RevanthAgeValidationException extends Throwable {
+    public RevanthAgeValidationException(String message)
+    {
+        super(message);
+    }
+}
---
+++ Day04/RevanthException/src/main/java/Custom/TestingAgeForVote.java
@@ -0,0 +1,21 @@
+package Custom;
+
+public class TestingAgeForVote {
+    public static void main(String[] args) {
+        TestingAgeForVote test=new TestingAgeForVote();
+        try{
+            test.validateAge(22);
+        }catch(RevanthAgeValidationException e){
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }finally{
+            System.out.println("This is executed");
+        }
+    }
+
+    public void validateAge(int age) throws RevanthAgeValidationException {
+        if (age <=18) {
+            throw new RevanthAgeValidationException("Age must be above 18 to vote.");
+        }
+    }
+}
