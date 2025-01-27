83f249061f703c3e7d2d907b69b195d5225d3d96
admin <vutukururevanth@gmail.com>
2025-01-26 20:39:26 -0800

    Day05

--- Day05/Calculator-App/src/main/java/com/revanth/app/MyCalculator.java
+++ Day05/Calculator-App/src/main/java/com/revanth/app/MyCalculator.java
@@ -1,6 +1,6 @@
 package com.revanth.app;
 
-public class Calculator {
+public class MyCalculator {
     public double add(double n1, double n2) {
         return n1+n2;
     }
@@ -11,5 +11,9 @@ public class Calculator {
 
     public double div(double n1,double n2){
         return n1/n2;
+    }
+
+    public double multiply(double n1,double n2){
+        return n1*n2;
     }
 }
--- Day05/Calculator-App/src/test/java/com/revanth/app/MyCalculatorTest.java
+++ Day05/Calculator-App/src/test/java/com/revanth/app/MyCalculatorTest.java
@@ -2,26 +2,33 @@ package com.revanth.app;
 
 import org.junit.Test;
 
-public class CalculatorTest {
+public class MyCalculatorTest {
     @Test
     public void testAdd(){
-        Calculator calc=new Calculator();
+        MyCalculator calc=new MyCalculator();
         double result=calc.add(2,5);
         assert result==7;
     }
 
     @Test
     public void testsub(){
-        Calculator calc=new Calculator();
+        MyCalculator calc=new MyCalculator();
         double result=calc.sub(10,4);
         assert result==6;
     }
 
     @Test
     public void testdiv(){
-        Calculator calc=new Calculator();
+        MyCalculator calc=new MyCalculator();
         double result=calc.div(25,5);
         assert result==5;
+    }
+
+    @Test
+    public void tsetMultiply(){
+        MyCalculator calc=new MyCalculator();
+        double result=calc.multiply(12,12);
+        assert result==144;
     }
 }
 
