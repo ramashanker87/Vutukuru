c5d79afe627d33c5bba9765d802b75ca8fba2ed0
admin <vutukururevanth@gmail.com>
2025-02-02 23:22:50 -0800

    Assignment

--- Day05/simple-spring-boot/src/test/java/com/revanth/app/SimpleSpringBootApplicationTests.java
+++ Day05/simple-spring-boot/src/test/java/com/revanth/app/SimpleSpringBootApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PatientManagementAppApplicationTests {
+class SimpleSpringBootApplicationTests {
 
 	@Test
 	void contextLoads() {
--- Day08/patient-management-app/src/main/java/com/revanth/app/model/Patient.java
+++ Day08/patient-management-app/src/main/java/com/revanth/app/model/Patient.java
@@ -24,17 +24,13 @@ public class Patient {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+
 
     public String getId() {
         return id;
     }
 
-    public void setId(String id) {
-        this.id = id;
-    }
+
 
     public String getHospitalName() {
         return hospitalName;
@@ -48,26 +44,12 @@ public class Patient {
         return age;
     }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
 
-    public String getGender() {
-        return gender;
-    }
 
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
 
-    @Override
-    public String toString() {
-        return "Patient{" +
-                "name='" + name + '\'' +
-                ", id='" + id + '\'' +
-                ", hospitalName='" + hospitalName + '\'' +
-                ", age=" + age +
-                ", gender='" + gender + '\'' +
-                '}';
-    }
+
+
+
+
+
 }
