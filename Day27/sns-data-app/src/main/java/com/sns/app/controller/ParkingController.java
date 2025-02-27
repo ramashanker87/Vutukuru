c0a1463956080dfec130c5ecbabc889cce8afb95
admin <rama@gail.com>
2025-02-27 04:54:46 -0800

    day27

---
+++ Day27/sns-data-app/src/main/java/com/sns/app/controller/ParkingController.java
@@ -0,0 +1,33 @@
+package com.sns.app.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.sns.app.modle.Parking;
+import com.sns.app.modle.ParkingResponse;
+import com.sns.app.service.SNSServices;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import java.time.Instant;
+
+@RestController
+public class ParkingController {
+    private final SNSServices snsServices;
+
+    public ParkingController(SNSServices snsServices) {
+        this.snsServices = snsServices;
+    }
+
+    @PostMapping("/start/parking")
+    public ParkingResponse startParking(@RequestBody Parking parking) throws JsonProcessingException {
+        Instant startTime = Instant.now();
+        Instant endTime = startTime.plusSeconds(3600);
+        ParkingResponse parkingResponse=new ParkingResponse();
+        parkingResponse.setCarRegNo(parking.getCarRegNo());
+        parkingResponse.setParkingNo(parking.getParkingNo());
+        parkingResponse.setStartTime(startTime.toString());
+        parkingResponse.setEndTime(endTime.toString());
+        parkingResponse.setParkingStatus(parking.getParkingStatus());
+        snsServices.sendMessageToSns(parking);
+        return parkingResponse;
+    }
+}
